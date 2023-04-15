24560cf37e1f452ed00a0ddf38da240ef14486c6
Manpreet-SinghGEORGIAN <123345160+Manpreet-SinghGEORGIAN@users.noreply.github.com>
2023-04-15 14:42:32 -0400

    Add files via upload

---
+++ user.php
@@ -0,0 +1,45 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    
+
+    <title>Crud operation</title>
+  </head>
+  <body>
+    <div class="container my-5">
+    <form method="post">    <label>Name</label>
+    <input type="text" class="form-control" 
+    placeholder="Enter your name" name="name" autocomplete="off">
+  </div>
+  <div class="container"><div class="form-group">
+    <label>Email</label>
+    <input type="email" class="form-control" 
+    placeholder="Enter your email" name="email" autocomplete="off">
+  </div> 
+  <div class="container">
+    <div class="form-group">
+    <form method="post"> <label>Mobile</label>
+    <input type="text" class="form-control" 
+    placeholder="Enter your mobile number" name="email" autocomplete="off">
+  </div>
+  <div class="container">
+  <div class="form-group">
+  <label>Password</label>
+    <input type="text" class="form-control" 
+    placeholder="Enter your password" name="password">
+  </div>
+
+  <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+</form>
+    </div>
+     
+
+  </body>
+</html>
